b47069e84dd94d08839488c76eac20980886e88a
Sebastien Champvillard <cs212535@localhost.localdomain>
2023-09-25 10:32:00 +0200

    Vrai jeu de test

--- src/java/modele/Joueur.java
+++ src/java/modele/Joueur.java
@@ -2,6 +2,8 @@ package java.modele;
 
 public class Joueur 
 {
+    static int nbJoueur =0;
+    static String[] tabCouleur =  {"ROUGE","VERT","BLEU","JAUNE"};
     String nom;
     int argent;
     String couleur;
@@ -9,9 +11,15 @@ public class Joueur
 
     public Joueur()
     {
-        this.nom = "joueur test";
-        this.couleur = "couleur test";
+        this.couleur = tabCouleur[nbJoueur];
+        this.nom = "joueur " + Joueur.nbJoueur++ ;
         this.prison =0;
         this.argent = 20_000;
+    }
+
+    public static int lancerDes()
+    {
+        int r1 =(int) (1+Math.random()*5);
+        return r1;
     }
 }
--- src/java/modele/Test.java
+++ src/java/modele/Test.java
@@ -1,1 +1,10 @@
+package java.modele;
 
+/**
+ * Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        Jeu j = new Jeu(1);
+    }    
+}

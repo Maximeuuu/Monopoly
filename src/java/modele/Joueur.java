7388c629c92ac77e385f1364875c29b170a18d60
Sebastien Champvillard <cs212535@localhost.localdomain>
2023-09-25 09:56:51 +0200

    Joueur test

---
+++ src/java/modele/Joueur.java
@@ -0,0 +1,17 @@
+package java.modele;
+
+public class Joueur 
+{
+    String nom;
+    int argent;
+    String couleur;
+    int prison;
+
+    public Joueur()
+    {
+        this.nom = "joueur test";
+        this.couleur = "couleur test";
+        this.prison =0;
+        this.argent = 20_000;
+    }
+}

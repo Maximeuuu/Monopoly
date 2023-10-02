9ae2d12fe6e5d4e337f2112089cabc3c8e0345de
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2023-10-02 13:17:38 +0200

    Carte.java

--- src/modele/cartes/Carte.java
+++ src/modele/cartes/Carte.java
@@ -1,1 +1,46 @@
+//a supprimer peut être
+
 package monopoly.modele.cartes;
+
+import monopoly.modele;
+
+public class Carte
+{
+	protected String nom;
+	protected Image image;
+	
+	public Case( String nom )
+	{
+		this.nom = nom;
+	}
+	
+	public String getNom()
+	{
+		return this.nom;
+	}
+	
+	public int getNbJoueur()
+	{
+		return this.tabJoueur.size();
+	}
+	
+	public void add(Joueur joueur)
+	{
+		this.tabJoueur.add(joueur);
+	}
+	
+	public void remove(Joueur joueur)
+	{
+		this.tabJoueur.remove(joueur);
+	}
+	
+	public Joueur get(int i)
+	{
+		return this.tabJoueur.get(i);
+	}
+	
+	public void action()
+	{
+		
+	}
+}
